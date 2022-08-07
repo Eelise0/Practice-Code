dbac6b9f701e6c0b34ce61ffd8242a8bae2ef024
Eelise0 <106895470+Eelise0@users.noreply.github.com>
2022-08-07 12:00:18 -0700

    Updated file to show my Lab 2 progress

--- ConditionsAndLoopsLab2.js
+++ ConditionsAndLoopsLab2.js
@@ -23,17 +23,23 @@ let openDoor = true
 // using a conditional, determine if alarm is set. 
 // if alarm is set and door is set to open, print "Sound Alarm!" to the console
 // otherwise, print "Everything is fine." to the console.
-
-
-
+if (alarmSet && openDoor) { // *** there is no need to put in code alarmSet === false & openDoor === true
+    console.log('Sound Alarm!'); // *** because they are already boolean values so the code knows to read them
+} else {                           // *** as boolean when going through the expression.
+    console.log('Everything is fine.');
+}
 
 // create two variables, username and password
 // create a conditional, if the username is "Tommy123" and the password is "12345"
 // or the username is "Timmy456" and the password is "6789", print "Admin Login Successful" to the console
 // otherwise, print "Admin Access Denied"
-
-
-
+let username = 'Tbl'
+let password = '12222'
+if (username === 'Tommy123' && password === '12345' || username === 'Timmy456' && password === '6789') {
+    console.log('Admin Login Successful');
+}   else {
+    console.log('Admin Access Denied');
+}
 
 // write code that will set the value of studentClass based on studentGrade
 // studentGrade will be K-12
@@ -41,10 +47,39 @@ let openDoor = true
 // 7-8 will be Middle
 // 9 will be Freshman, 10 Sophomore, 11 Junior, 12 Senior
 // Anything other than these values will return "Error" to the console
+let studentGrade = 11;
+let studentClass;
 
-
-
-
+switch (studentGrade) {
+    case 'K':
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        studentClass = 'Elementary';
+        break;
+    case 7:
+    case 8:
+        studentClass = 'Middle';
+        break;
+    case 9:
+        studentClass = 'Freshman';
+        break;
+    case 10:
+        studentClass = 'Sophmore';
+        break;
+    case 11:
+        studentClass = 'Junior';
+        break;
+    case 12:
+        studentClass = 'Senior';
+        break;  
+    default:
+        console.log('Error');
+}
+console.log(studentClass);
 
 // write a for loop that will iterate backwards from 10 to -10
 
