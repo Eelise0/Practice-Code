1935510d747f673827593cb427fc4f533a34e3a8
Eelise0 <106895470+Eelise0@users.noreply.github.com>
2022-08-14 12:32:17 -0700

    Update HTML and JS files from Week 3, functions

---
+++ scriptW3.js
@@ -0,0 +1,29 @@
+/* ARRAYS PRACTICE */
+
+// let customerNames = [];
+// customerNames.push('Elise Schmidt');
+// customerNames.push('Ryan Joseph');
+// customerNames.push('Justine Anderson');
+
+// for (let i = 0; i < customerNames.length; i++) {
+//     console.log(customerNames[i]);
+// }
+
+// for (name of customerNames) {
+//     console.log(name);
+// }
+
+/* FUNCTIONS PRACTICE */
+
+// function myFunction () {
+//     for (let i = 0; i < 100; i++) {
+//         console.log(i);
+//     }
+// }
+// myFunction();
+
+function createFullName(firstName, lastName) {
+    return firstName + ' ' + lastName;
+}
+createFullName ('Elise', 'Schmidt');
+createFullName ('Ryan', 'Joseph');
